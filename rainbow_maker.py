00bb7d58d844b78c31b986cc143cd794b868811e
Aves <aves@localhost.localdomain>
2020-03-12 08:42:43 -0500

    improvements to rainbow_maker.py

--- rainbow_maker.py
+++ rainbow_maker.py
@@ -9,7 +9,7 @@ import hashlib, gzip, pickle, optparse
 from os.path import isdir, isfile, join
 from itertools import islice
 
-INCREMENT = 100000
+INCREMENT = 1000000
 Metadict = {}
 
 
@@ -38,32 +38,49 @@ def rainbow_maker(file, function, dire, salt_prefix=None, salt_suffix=None):
                     )
                 lines_read = length
             #
-            first_4 = sorted(list(megadict.keys()))[:4]
-            Metadict[first_4] = megadict
+            first_8 = sorted(list(megadict.keys()))[:8]
+            Metadict[first_8] = megadict
     #
     with open(join(dire, 'rainbow_table_' + str(function.__name__)), 'wb') as g:
-        g.write(bytes(gzip.compress(pickle.dumps(Metadict))))
+        if length > 1000000:
+            g.write(bytes(gzip.compress(pickle.dumps(Metadict))))
+        else:
+            g.write(bytes(pickle.dumps(Metadict)))
 
 
 if __name__ == '__main__':
-    path = input('Enter the path to the password text file:\n')
+    parser = optparse.OptionParser(
+        'usage: %prog -f <password_file> -d <directory_to_save_to> [-p <salt_prefix> | -u <salt_suffix>]'
+    )
+    parser.add_option('-f', dest='path', type='string', help='path to password file')
+    parser.add_option('-d', dest='dire', type='string', help='directory to save rainbow table to')
+    parser.add_option('-p', dest='salt_prefix', type='string', help='salt prefix, if applicable')
+    parser.add_option('-u', dest='salt_suffix', type='string', help='salt suffix, if applicable')
+    (options, args) = parser.parse_args()
+    path = options.path
+    dire = options.dire
     if not isfile(path):
         raise FileNotFoundError(path, 'is not a file')
+    if not isdir(dire):
+        raise NotADirectoryError(dire, 'is not a directory')
+    salt_prefix = options.salt_prefix if options.salt_prefix else None
+    salt_suffix = options.salt_suffix if options.salt_suffix else None
+    try:
+        assert not (salt_prefix and salt_suffix)
+    except AssertionError:
+        print('Cannot have both salt prefix and suffix.')
+        exit(1)
     #
     print('\nChoose a hash function to use:')
     count = 1
-    options = {}
+    hash_funcs = {}
     for x in hashlib.algorithms_available:
         print(str(count) + ') ' + x)
-        options[count] = x
+        hash_funcs[count] = x
         count += 1
     choice = int(input('\n# of Choice: '))
     if choice not in range(1, len(hashlib.algorithms_available) + 1):
         raise ValueError('Invalid choice #')
+    function = getattr(hashlib, hashfuncs[choice])
     #
-    dire = input('\nEnter the directory path to save the results to:\n')
-    if not isdir(dire):
-        raise NotADirectoryError(dire, 'is not a directory')
-    #
-
-    rainbow_maker(path, options[choice], dire, salt_prefix, salt_suffix)
+    rainbow_maker(path, function, dire, salt_prefix, salt_suffix)
